46688a95874ac2e52bff27505d92b5d2a5813255
Emiiiiiiiiiiii <emi.gallardo@duocuc.cl>
2026-06-28 19:45:54 -0400

    Configura RestTemplate en certificacion

--- certificacion/src/main/java/com/pizzas/certificacion/AppConfig.java
+++ certificacion/src/main/java/com/pizzas/certificacion/AppConfig.java
@@ -4,9 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+// Configuración general del microservicio certificación
 @Configuration
-
 public class AppConfig {
+    // Crea el RestTemplate para comunicarse con otros microservicios
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
